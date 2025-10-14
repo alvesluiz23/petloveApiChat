63ba2b2da21ca79bd99f29de9152d0a594509f7f
Luiz <alvesluizeduardo23@gmail.com>
2025-10-14 12:34:36 -0300

    Adicionando codigo retorno endpoints, verificacao payload e gitignore

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ app = Flask(__name__)
 def question_and_answer():
     if request.method == "POST":
         body = request.get_json()
+        if('question' not in body):
+            return "Passar o campo question no json", 422
         return {
             "response": generate_answer(body['question'])
-        }
-app.run()
+        }, 200
+
+
+if( __name__ == '__main__'):
+    app.run()
